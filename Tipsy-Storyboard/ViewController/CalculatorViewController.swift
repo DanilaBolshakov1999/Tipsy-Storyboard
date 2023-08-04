23e3ccab419ccb1a476e4b8f20cf4ef1927061a8
Danila Bolshakov <danila1999777@gmail.com>
2023-08-04 10:51:58 +0300

    bugfix

--- Tipsy-Storyboard/ViewController/CalculatorViewController.swift
+++ Tipsy-Storyboard/ViewController/CalculatorViewController.swift
@@ -7,22 +7,26 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
     
-    @IBOutlet weak var billTextField: UITextField!
-    @IBOutlet weak var zeroPctButton: UIButton!
-    @IBOutlet weak var tenPctButton: UIButton!
-    @IBOutlet weak var twentyPctButton: UIButton!
-    @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var zeroPctButton: UIButton!
+    @IBOutlet var tenPctButton: UIButton!
+    @IBOutlet var twentyPctButton: UIButton!
+    @IBOutlet var splitNumberLabel: UILabel!
     @IBOutlet var buttonCalculate: UIButton!
     
-    var tip = 0.10
-    var numberOfPeople = 2
-    var billTotal = 0.0
-    var finalResult = "0.0"
+    private var tip = 0.10
+    private var numberOfPeople = 2
+    private var billTotal = 0.0
+    private var finalResult = "0.0"
     
     override func loadView() {
         super.loadView()
+        buttonLayer()
+    }
+    
+    private func buttonLayer() {
         buttonCalculate.layer.cornerRadius = 20
     }
     
@@ -45,25 +49,22 @@ class CalculatorViewController: UIViewController {
         numberOfPeople = Int(sender.value)
     }
     
-    @IBAction func calculatePressed(_ sender: UIButton) {
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
         print(numberOfPeople)
         let bill = billTextField.text!
         if bill != "" {
             billTotal = Double(bill)!
             let result = billTotal * (1 + tip) / Double(numberOfPeople)
             finalResult = String(format: "%.1f", result)
-            print(finalResult)
         }
-        self.performSegue(withIdentifier: "goToResults", sender: self)
+        performSegue(withIdentifier: "goToResults", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "goToResults" {
             let destinationVC = segue.destination as! ResultViewController
-            destinationVC.result = finalResult
-            destinationVC.tip = Int(tip * 100)
-            destinationVC.split = numberOfPeople
+            destinationVC.configure(result: finalResult, tip: Int(tip * 100), split: numberOfPeople)
         }
     }
 }
